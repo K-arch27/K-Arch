c86b82b00897b44b3a7ff88137ccafd87be3aea5
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-31 01:59:06 -0400

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-cd $SCRIPT_DIR/airootfs/root
-rm -Rfd archscript
-git clone -b K-Arch-Iso https://github.com/k-arch27/archscript
-mkarchiso -v $SCRIPT_DIR
