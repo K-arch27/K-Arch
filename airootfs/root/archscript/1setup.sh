7619bbaec55a5c8dbdbcdc475e9e3d8818e6275b
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-31 14:41:07 -0400

    Update 1setup.sh

--- airootfs/root/archscript/1setup.sh
+++ airootfs/root/archscript/1setup.sh
@@ -448,7 +448,7 @@ function rootpartition() {
     pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
     pacman-key --lsign-key FBA220DFC880C036
     pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
-    cat /root/mirror.txt >> /etc/pacman.conf
+    cat /root/archscript/mirror.txt >> /etc/pacman.conf
     pacman -Sy  chaotic-keyring --needed --noconfirm
     timedatectl set-ntp true
     reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
