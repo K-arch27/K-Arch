4488bace49e57a9aa38217df15de1e78bd449e1e
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-31 02:33:38 -0400

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -8,8 +8,7 @@ iso_application="K-Arch Btrfs Installer"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+bootmodes=('uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
            'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
