7e51a8b2a6c668986a70ebe929535d8799587f16
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-04-01 15:21:34 -0400

    Update start.sh
    Added A prompt asking the user if ready to start the installation

--- airootfs/root/archscript/start.sh
+++ airootfs/root/archscript/start.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 clear
+    # Prompt the user with a clickable option to check if they are ready
+   if [ zenity --question --text="Are you ready to Install?" --ok-label="No" --cancel-label="yes" ]; then
+        zenity --info --text="The script can be launched back with Right clic on the desktop"
+        exit
+   fi
+   
 echo -ne "
 -------------------------------------------------------------------------
                  █████╗ ██████╗  ██████╗██╗  ██╗
