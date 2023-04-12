2c1c45c83893973763dea974d2b69175b17fa87b
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-04-12 16:19:41 -0400

    Update start.sh

--- airootfs/root/archscript/start.sh
+++ airootfs/root/archscript/start.sh
@@ -1,9 +1,67 @@
 #!/bin/bash
 
+
+echo -ne "
+-------------------------------------------------------------------------
+                 █████╗ ██████╗  ██████╗██╗  ██╗
+                ██╔══██╗██╔══██╗██╔════╝██║  ██║
+                ███████║██████╔╝██║     ███████║
+                ██╔══██║██╔══██╗██║     ██╔══██║
+                ██║  ██║██║  ██║╚██████╗██║  ██║
+                ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
+-------------------------------------------------------------------------
+        Automated Arch Linux Installer With Btrfs Snapshot
+-------------------------------------------------------------------------
+"
+    source /root/archscript/config.sh
+    chmod +x /root/archscript/startup.sh
+    chmod +x /root/archscript/btrfs.sh
+    chmod +x /root/archscript/1setup.sh
+    chmod +x /root/archscript/2partition.sh
+    chmod +x /root/archscript/3strap.sh
+    chmod +x /root/archscript/4chroot.sh
+    chmod +x /root/archscript/5final.sh
+
+
     # Prompt the user with a clickable option to check if they are ready
-   if [ zenity --question --text="Are you ready to Install?" --ok-label="No" --cancel-label="yes" ]; then
+   if [ zenity --question --text="What do you want to do ?" --ok-label="No" --cancel-label="yes" ]; then
         zenity --info --text="The script can be launched back with Right click on the desktop"
         exit
    fi
    
    
+   
+   
+# Display Zenity dialog box with three buttons
+zenity --question --title="Choose an option" --text="Select an option:" \
+       --ok-label="Install Arch Linux" --cancel-label="Use the Btrfs Layout Tool only" --extra-button="Close For Now"
+
+# Capture the exit status of the dialog box
+result=$?
+
+# Check exit status and take appropriate action
+case $result in
+    0)
+        # Action for Option 1 (OK button)
+    ( bash /root/archscript/startup.sh )|& tee /root/archscript/startup.log
+    zenity --info --title="Done" --text="You can now reboot or use chroot and customize your system" --ok-label="OK"
+    exit
+        ;;
+    1)
+        # Action for Option 2 (Cancel button)
+    ( bash /root/archscript/btrfs.sh )|& tee /root/archscript/btrfs.log
+    ( bash /root/archscript/2partition.sh )|& tee /root/archscript/partition.log
+    zenity --info --title="Done" --text="You can now proceed with a normal manual install" --ok-label="OK"
+    exit
+        ;;
+    2)
+        # Action for Option 3 (Extra button)
+        echo "Option 3 selected"
+        zenity --info --title="When Ready" --text="You can Launch Back the install / Layout tool by right clicking the desktop" --ok-label="OK"
+        exit
+        ;;
+    *)
+        # No or invalid choice
+        exit
+        ;;
+esac
