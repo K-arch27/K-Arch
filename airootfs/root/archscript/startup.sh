a7f47fab9f018eda38a27f0b67694b1153133d59
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-04-12 16:04:59 -0400

    Update and rename start.sh to startup.sh

--- airootfs/root/archscript/startup.sh
+++ airootfs/root/archscript/startup.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 clear
-    # Prompt the user with a clickable option to check if they are ready
-   if [ zenity --question --text="Are you ready to Install?" --ok-label="No" --cancel-label="yes" ]; then
-        zenity --info --text="The script can be launched back with Right click on the desktop"
-        exit
-   fi
-   
+
 echo -ne "
 -------------------------------------------------------------------------
                  █████╗ ██████╗  ██████╗██╗  ██╗
