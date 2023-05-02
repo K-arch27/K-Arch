8a264ad33eb455c809af83f42e8dcda6ab6ef41a
K-arch27 <kaida.arch27@gmail.com>
2023-05-01 21:26:56 -0400

    Update 4chroot.sh

--- airootfs/root/archscript/4chroot.sh
+++ airootfs/root/archscript/4chroot.sh
@@ -157,7 +157,7 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 
-if [ FIRMWARE_TYPE = "UEFI" ]; then
+if [ "$FIRMWARE_TYPE" = "UEFI" ]; then
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch-Btrfs --modules="normal test efi_gop efi_uga search echo linux all_video gfxmenu gfxterm_background gfxterm_menu gfxterm loadenv configfile gzio part_gpt btrfs"
 else
 grub-install --target=i386-pc $ROOTDEV --modules="normal test echo linux all_video gfxmenu gfxterm_background gfxterm_menu gfxterm loadenv configfile gzio part_gpt btrfs"
