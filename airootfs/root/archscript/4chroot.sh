94518eea4736950229d78ff1c94db6a658d3dec6
K-arch27 <kaida.arch27@gmail.com>
2023-05-03 17:54:15 -0400

    Update 4chroot.sh
     Merged last part of the script inside

--- airootfs/root/archscript/4chroot.sh
+++ airootfs/root/archscript/4chroot.sh
@@ -26,14 +26,24 @@ sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 #Add parallel downloading
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
-if [ "$LIBCHOICE" = "yes" ]; then 
-#Enable multilib
-sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-pacman -Sy --noconfirm
-fi
 
-clear
-logo
+mkinitcpio -P
+
+
+-------------------------------------------------------------------------
+                    Setup hostname and timezone
+-------------------------------------------------------------------------
+"
+echo "$NAME_OF_MACHINE" > /etc/hostname
+ln -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+hwclock --systohc 
+# Set keymaps
+echo KEYMAP=$KEYMAP > /etc/vconsole.conf
+loadkeys $KEYMAP
+echo "LANG=${LANGLOCAL}" > /etc/locale.conf
+
+
+
 echo -ne "
 -------------------------------------------------------------------------
                     Adding User
@@ -59,15 +69,7 @@ fi
 echo "$USERNAME:$PASSWORD" | chpasswd
 echo "root:$ROOTPASSWORD" | chpasswd
 
-mkinitcpio -P
 
-umount /.snapshots
-rm -r /.snapshots
-snapper --no-dbus -c root create-config /
-btrfs subvolume delete /.snapshots
-mkdir /.snapshots
-mount -a
-chmod 750 /.snapshots
 
 clear
 logo
@@ -103,8 +105,65 @@ if [ "$DECHOICE" = "kaidaplasma" ]; then
 
 fi
 
-clear
-logo
+
+echo -ne "
+-------------------------------------------------------------------------
+                  Extra Repo
+-------------------------------------------------------------------------
+"
+
+
+if [ "$LIBCHOICE" = "yes" ]; then 
+  sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+  pacman -Sy --noconfirm
+fi
+
+if [ "$CHAOCHOICE" = "yes" ]; then
+  pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+  pacman-key --lsign-key FBA220DFC880C036
+  pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+  cat /root/archscript/mirror.txt >> /etc/pacman.conf
+fi
+
+if [ "$BLACKCHOICE" = "yes" ]; then
+  curl -O https://blackarch.org/strap.sh
+  chmod +x strap.sh
+  bash strap.sh
+  rm /strap.sh
+fi
+
+echo -ne "
+-------------------------------------------------------------------------
+                  Snapper Subvolume setup
+-------------------------------------------------------------------------
+"
+
+umount /.snapshots
+rm -r /.snapshots
+snapper --no-dbus -c root create-config /
+btrfs subvolume delete /.snapshots
+mkdir /.snapshots
+mount -a
+chmod 750 /.snapshots
+
+#Changing The timeline auto-snap
+sed -i 's|QGROUP=""|QGROUP="1/0"|' /etc/snapper/configs/root
+sed -i 's|NUMBER_LIMIT="50"|NUMBER_LIMIT="5-15"|' /etc/snapper/configs/root
+sed -i 's|NUMBER_LIMIT_IMPORTANT="50"|NUMBER_LIMIT_IMPORTANT="5-10"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_LIMIT_HOURLY="10"|TIMELINE_LIMIT_HOURLY="2"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_LIMIT_DAILY="10"|TIMELINE_LIMIT_DAILY="2"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_LIMIT_WEEKLY="0"|TIMELINE_LIMIT_WEEKLY="2"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_LIMIT_MONTHLY="10"|TIMELINE_LIMIT_MONTHLY="0"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_LIMIT_YEARLY="10"|TIMELINE_LIMIT_YEARLY="0"|' /etc/snapper/configs/root
+
+
+#activating the auto-cleanup
+SCRUB=$(systemd-escape --template btrfs-scrub@.timer --path /dev/disk/by-uuid/${ROOTUUID})
+systemctl enable ${SCRUB}
+systemctl enable snapper-timeline.timer
+systemctl enable snapper-cleanup.timer
+
+
 echo -ne "
 -------------------------------------------------------------------------
                   Grub Install
@@ -126,3 +185,22 @@ sed -i 's|,subvolid=258,subvol=/@/.snapshots/1/snapshot| |' /etc/fstab
 
 
 grub-mkconfig -o /boot/grub/grub.cfg
+
+
+
+logo
+echo -ne "
+
+
+
+-------------------------------------------------------------------------
+        Updating full system 
+-------------------------------------------------------------------------
+"
+
+
+pacman -Syyu --noconfirm
+
+
+
+
